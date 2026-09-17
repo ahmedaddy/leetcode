f9d771a81624a11c39841fb9298657960c346c88
ahmed addy <130843411+ahmedaddy@users.noreply.github.com>
2026-09-17 17:40:25 +0100

    Time: 7 ms (73.89%), Space: 12.4 MB (21.8%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -7,7 +7,7 @@ class Solution(object):
         roman = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         acc = 0
         prev = 0
-        for c in s[::-1]:
+        for c in reversed(s):
             if prev > roman[c]:
                 acc -= roman[c]
             else:

00965fd6c4355a6900333a37d10db4c2e858fa4e
ahmed addy <130843411+ahmedaddy@users.noreply.github.com>
2026-09-18 17:24:46 +0100

    Time: 139 ms (19.33%), Space: 18.9 MB (97.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        buy1 = float("inf")
+        buy = float("inf")
         profit = 0
 
         for price in prices:
-            buy1 = min(buy1, price)
-            profit = max(profit, price - buy1)
+            buy = min(buy, price)
+            profit = max(profit, price - buy)
         return profit
